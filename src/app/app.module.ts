cf7f2ea0bd4593d860de67643534c92888145405
michelmartins9164 <michelmartins9164@gmail.com>
2024-01-27 01:52:11 -0300

    mudando key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire';
+import {AngularFireModule} from '@angular/fire'
 import { environment } from 'src/environments/environment';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatCardModule } from '@angular/material/card';
+import {MatCardModule} from '@angular/material/card';
 import { SearchComponent } from './components/search/search.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcygoeh2ZOaQ1jymneFewgArI-F860rms",
+  authDomain: "bfc-jiujitsu.firebaseapp.com",
+  projectId: "bfc-jiujitsu",
+  storageBucket: "bfc-jiujitsu.appspot.com",
+  messagingSenderId: "502436994329",
+  appId: "1:502436994329:web:3da37c9ac973764bb9b988",
+  measurementId: "G-2ZH5X8H94N"
+}
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SearchComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp({
-      firebaseConfig: {
-        apiKey: 'AIzaSyAcygoeh2ZOaQ1jymneFewgArI-F860rms',
-        authDomain: 'bfc-jiujitsu.firebaseapp.com',
-        projectId: 'bfc-jiujitsu',
-        storageBucket: 'bfc-jiujitsu.appspot.com',
-        messagingSenderId: '502436994329',
-        appId: '1:502436994329:web:3da37c9ac973764bb9b988',
-        measurementId: 'G-2ZH5X8H94N',
-      },
-    }),
+    AngularFireModule.initializeApp(firebaseConfig),
     BrowserAnimationsModule,
     MatCardModule,
     MatInputModule,
     MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
